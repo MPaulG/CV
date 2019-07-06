6e0fd32c6723443eda4777428f71ca300e4acbb3
MPaulG <52612139+MPaulG@users.noreply.github.com>
2019-07-06 17:40:50 -0500

    Goal Information

---
+++ Goal.ml
@@ -0,0 +1,11 @@
+<Goal>The aim for This Project 
+                      is to design a webpage where 
+                      potential employers can read
+                      information about my past. 
+      <Personal>I 
+                      include work with personal 
+                      interests as well as links 
+                      to online examples when they
+                      still exist. 
+      </Personal>
+</Goal>
