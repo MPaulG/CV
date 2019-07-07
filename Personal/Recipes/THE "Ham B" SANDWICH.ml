58f34cde6d7b8c38780d1346ab531a0397aa63ae
Matt <MPaulG@gmail.com>
2019-07-07 17:22:03 -0500

    Added Sandwich Recipe

---
+++ Personal/Recipes/THE "Ham B" SANDWICH.ml
@@ -0,0 +1,10 @@
+<Sandwich>
+  <Ingredients>
+    <1> One Onion Bagel </>
+    <2> Four Slices of Uncured Virginia Ham </>
+    <3> Baby Spinach (remove stems) </>
+    <4> Garlic & Herb cream cheese </>
+    <5> Mayo made with Olive Oil </>
+    <6> Spicy Chipotle Mayo </>
+  </>
+</>
