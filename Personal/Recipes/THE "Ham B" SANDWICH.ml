e59e668494060ac27a8445bf7243dbef92e879cc
Matt <MPaulG@gmail.com>
2019-07-07 17:55:17 -0500

    Change Ham B sandwich to include instructions

--- Personal/Recipes/THE "Ham B" SANDWICH.ml
+++ Personal/Recipes/THE "Ham B" SANDWICH.ml
@@ -7,4 +7,15 @@
     <5> Mayo made with Olive Oil </>
     <6> Spicy Chipotle Mayo </>
   </>
+  <Recipe>
+    <Steps>
+      <1>Toast top side of Onion bagel</>
+      <2>Layer bottom half of bagel with mayo made with Olive Oil</>
+      <3>Add slices of ham to bottom over mayo layer</>
+      <4>Add layer of chipotle mayo over ham</>
+      <5>Add baby spinach with stems removed over chipotle mayo</>
+      <6>Add layer of cream cheese to toasted top side of bagel</>
+      <7>Combine top of bagel coated with cream cheese to bottom part of bagel on top of baby spinach and chipotle mayo</>
+    </>
+  </>
 </>
