1f9848088ec098b66d2a61981d12ccb230701246
Matt <MPaulG@gmail.com>
2019-07-10 15:12:09 -0500

    Wrote Circle Intro

---
+++ Code/Circle/CircleTree.ml
@@ -0,0 +1,17 @@
+<Circle>
+  <Idea>
+    <quote>Start with the idea of a circle</quote>
+
+    <question>How do we make a circle?</question>
+
+    <question>Does a circle need to be visual or can it also be theoretical?</question>
+      <questionResponse>One idea that came to my mind that can be represented right now is A to B to A</questionResponse>
+        <questionResponseContinued>A to B to A does not HAVE to be a circle, it can also be represented in other ways such as a line or even a point or any sort of mind map.</questionResponseContinued>
+
+    <question>What existing ways of making a visual circle do we know of that we can make using software?</question>
+      <questionResponse>I know how to make a visual circle by writing HTML code and styling with CSS code.</questionResponse>
+        <questionResponseLocation>See the file "HTMLCSSCircle.html" in the same directory as this file for an example</questionResponseLocation>
+      <questionResponse>I know how to make a visual circle using graphic software such as Inkscape and GIMP.</questionResponse>
+      <questionResponse>I know how to make a visual circle with other proprietary software.</questionResponse>
+ </Idea>
+</Circle>
