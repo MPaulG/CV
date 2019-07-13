5d1efcf1cb593198d7750761ec9770ed200ad001
Matt <MPaulG@gmail.com>
2019-07-13 17:54:54 -0500

    Described to fours place

---
+++ Code/LevelsInBinary/Comdecompression.m
@@ -0,0 +1,52 @@
+Levels in binary
+This work is copyright 2019 Matthewsyz. Do not copy the algorithm in any work unless in connection with Matthewsyz Levels in binary compression or decompression.
+
+PLACE
+VALUES of POSSIBILITY
+o
+P
+
+EXPORT FORMAT
+CONCATENATED FORMAT
+
+Ones place
+0
+1
+
+Export as $
+Concat as $
+
+Twos place
+01,S
+10,S
+
+Export as $$,S
+Concat as $$S
+
+Threes place
+01,SS,S
+10,SS,S
+11,SS,S
+
+Export as $$,SS,S
+Concat as $$SSS
+
+Fours place
+001,SS,SS,S
+010,SS,SS,S
+011,SS,SS,S
+100,SS,SS,S
+
+Export as $$$,SS,SS,S
+Concat as $$$SSSSSS
+
+/********************************
+Max values for place in decimal
+$,$,$,$,$,$,$,$,$
+9,8,7,6,5,4,3,2,1
+
+Max values for place in binary
+$$$$,$$$$,$$$,$$$,$$$,$$$,$$,$$,$
+1001,1000,111,110,101,100,11,10,1
+********************************/
+
